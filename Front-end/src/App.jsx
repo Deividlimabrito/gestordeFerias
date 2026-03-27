80a6c3e3c7c8e245ee757ebcf6e81d7f8a08181f
Deividlimabrito <deividlima1320@gmail.com>
2026-03-27 10:56:29 -0300

    atualizei a tela de login e criei a tela de recuperação de senha

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import SidebarLayout from "./components/Sidebar";
+import ForgotPassword from "./components/ForgotPassword";
 
 function App() {
   return (
-    // O Sidebar já contém a estrutura do menu e o container <main> ao lado dele
-    <div className="antialiased text-slate-900 bg-slate-50 min-h-screen">
-      <Sidebar />
-    </div>
+    <Router>
+      <Routes>
+        {/* Quando abrir o site, vai direto para o Login */}
+        <Route path="/" element={<Login />} />
+        
+        {/* 2. Nova rota para recuperação de senha */}
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
+        
+        {/* Quando o login for feito, ele vai para cá */}
+        <Route path="/dashboard" element={<SidebarLayout />} />
+
+        {/* Se digitar qualquer outra coisa, volta para o Login */}
+        {/* IMPORTANTE: Esta rota deve ser sempre a ÚLTIMA */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
